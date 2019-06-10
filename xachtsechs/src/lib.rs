12cce31fbeb1c0479bf3623c4d0e60879432fc7d
Joshua Worth <joshua@worth.id.au>
2019-06-10 17:56:18 +0200

    Converted to library and moved all DOS related code to a new project called libpseudos

---
+++ xachtsechs/src/lib.rs
@@ -0,0 +1,8 @@
+
+pub mod machine8086;
+pub mod types;
+
+// Super useful: http://www.mlsite.net/8086/
+// https://en.wikibooks.org/wiki/X86_Assembly/Machine_Language_Conversion#Mod_/_Reg_/_R/M_tables
+// https://www.felixcloutier.com/x86/rcl:rcr:rol:ror
+// https://sites.google.com/site/microprocessorsbits/processor-control-instructions/lock
